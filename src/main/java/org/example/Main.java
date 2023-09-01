cd48ca3ff5dd7bfd8f2eab7453a0dc9781c4b1df
lonchik2020 <krasleo@gmail.com>
2023-09-01 10:33:50 +0300

    add str good

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,9 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
 
+        System.out.println("good");
+
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
